34e5b2eb20c66e33509b5b3a7fef9062f01bd6e1
vinayakchachra <59107652+vinayak8295@users.noreply.github.com>
2023-01-18 14:34:45 +0530

    bug fixed
    now its able to do operation on 2 number but if more than 2 numbers are there it will not be able to perform

--- index.js
+++ index.js
@@ -1,71 +1,86 @@
 console.log("set");
-let answer=0,DisplayScreen='',symbol = '+';
+let answer=0,DisplayScreen='',symbol = 'd',ini=0,firstval=0;
+
+function helper(num1,num2,operator){
+  let output = 0;
+ 
+  try {
+
+    switch(operator) {
+      case '+':
+        output = num1 + num2
+        break;
+
+      case '*':
+        output = num1* num2
+        break;
+
+      case '-':
+        output = num1 - num2
+        break;
+
+     case '/': 
+        if (b === 0) {
+          throw "Can't divide by 0!"
+        } else {
+          output = num1 / num2
+        }
+        break;
+    }
+  }
+  catch(e) {
+    console.log("There's an error: ", e)
+  }
+
+  return output;
+}
+
+function cal(num){
+  console.log("numss");
+  console.log(num);
+    let val1=0,val2=0,flag=true;
+    for(let i=0;i<num.length;i++){
+    
+      if(num[i]=="+" || num[i]=="-" || num[i]=="*" || num[i]=="/"){
+        symbol=num[i];
+        flag=false;
+      }else{
+      if(flag){
+        val1= val1 + num[i];
+      }else{
+        val2 = val2 + num[i];
+      }
+      }
 
 
+    }
+    console.log("values");
+    console.log(val1,val2,symbol);
+    return helper(+val1,+val2,symbol);
+    
 
 
+}
 
 function storeVal(EnteredValue){
-    if(+EnteredValue){
-        DisplayScreen = ''+ DisplayScreen + EnteredValue;
-        document.getElementById("output").innerHTML = DisplayScreen;
-        let amount = EnteredValue;
-        console.log(amount);
-
-        switch (symbol) {
-            case '+':
-                answer=answer+ +EnteredValue;
-              break;
-            case "-":
-                answer=answer- +EnteredValue;
-              break;
-            case '*':
-                answer=answer* +EnteredValue;
-              break;
-            case '/':
-                answer=answer/ +EnteredValue;
-              break;    
-            default:
-                console.log( "Sw" );
-          }
-
-
+  if(EnteredValue=='C'){
+    DisplayScreen=0;
+    document.getElementById("output").innerHTML = DisplayScreen;
+    answer=0;
+  }else
+  if(EnteredValue=='='){
+    answer=cal(DisplayScreen);
+    document.getElementById("output").innerHTML = answer;
+    DisplayScreen=answer;
+    console.log(answer);
+  }else{
+    if(DisplayScreen=='0'){
+      DisplayScreen='' + EnteredValue;
     }else{
-       
-        DisplayScreen = ''+DisplayScreen+EnteredValue;
-        
-        document.getElementById("output").innerHTML = DisplayScreen;
-       
-       
-       console.log(EnteredValue);
-        switch (EnteredValue) {
-            case '+':
-                symbol = '+';
-              break;
-            case "-":
-                symbol = '-';
-              break;
-            case "*":
-                symbol = '*';
-              break;
-            case "/":
-                symbol = '/';
-              break;
-            case "C":
-                symbol = '+';
-                answer=0;
-                document.getElementById("output").innerHTML = 0;
-                DisplayScreen='';
-              break;
-            case '=':
-                console.log(answer);
-                DisplayScreen = answer;
-                document.getElementById("output").innerHTML = answer;
-                break;
-            default:
-                console.log( "try again" );
-          }
-    }
-    
-    
+    DisplayScreen = ''+ DisplayScreen + EnteredValue;
+  }
+  document.getElementById("output").innerHTML = DisplayScreen;
+  console.log(DisplayScreen);
+  }
+
 }
